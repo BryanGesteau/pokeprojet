9dc220380a0c8dbe189f0ba6d734155d5b31cb85
DI LORETO Romain <eldrago60@gmail.com>
2021-01-11 14:48:59 +0100

    Modification de la page d'acceuil

--- config/routes.php
+++ config/routes.php
@@ -55,7 +55,8 @@ $routes->scope('/', function (RouteBuilder $builder) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, templates/Pages/home.php)...
      */
-    $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    //$builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);//ancienne ligne
+    $builder->connect('/', ['controller' => 'Pokemons', 'action' => 'index']);//nouvelle ligne
 
     /*
      * ...and connect the rest of 'Pages' controller's URLs.
